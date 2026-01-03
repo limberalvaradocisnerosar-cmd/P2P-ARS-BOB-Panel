b0c045127d74a7fcd833b2d3b9ec4b5dfeeed819
limber <limberalvaradocisnerosar@gmail.com>
2026-01-03 19:58:29 -0300

    Fix: logger.js - Definir IS_DEV localmente para evitar error de importacion

--- assets/js/logger.js
+++ assets/js/logger.js
@@ -1,4 +1,4 @@
-import { IS_DEV } from './config.js';
+const IS_DEV = typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1');
 
 export const log = (...args) => IS_DEV && console.log(...args);
 export const warn = (...args) => IS_DEV && console.warn(...args);
